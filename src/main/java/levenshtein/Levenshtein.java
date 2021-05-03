7dd65737a62ce00c73562f340d0d788c714300e9
huisheng.jin <huisheng.jinge@gmail.com>
2021-05-03 21:42:09 +0800

    feat: insertion and substitution

--- src/main/java/levenshtein/Levenshtein.java
+++ src/main/java/levenshtein/Levenshtein.java
@@ -5,14 +5,47 @@ public class Levenshtein {
         if (word1.equals(word2)) {
             return 0;
         }
+        if (reachableByOneStep(word1, word2)) {
+            return 1;
+        }
+        return -1;
+    }
 
-        for (int i = 0; i < word1.length(); i++) {
-            if (deleteCharAt(word1, i).equals(word2)) {
-                return 1;
+    private boolean reachableByOneStep(String word1, String word2) {
+        return reachableByOneSubstitution(word1, word2)
+                || reachableByOneInsertion(word1, word2)
+                || reachableByOneDeletion(word1, word2);
+    }
+
+    private boolean reachableByOneSubstitution(String word1, String word2) {
+        if (word1.length() == word2.length()) {
+            int count = 0;
+            for (int i = 0; i < word2.length(); i++) {
+                if (word1.charAt(i) != word2.charAt(i)) {
+                    count++;
+                }
+            }
+            return count == 1;
+        }
+        return false;
+    }
+
+    private boolean reachableByOneInsertion(String word1, String word2) {
+        for (int i = 0; i < word2.length(); i++) {
+            if (deleteCharAt(word2, i).equals(word1)) {
+                return true;
             }
         }
+        return false;
+    }
 
-        return -1;
+    private boolean reachableByOneDeletion(String word1, String word2) {
+        for (int i = 0; i < word1.length(); i++) {
+            if (deleteCharAt(word1, i).equals(word2)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private String deleteCharAt(String word, int at) {
--- src/test/java/levenshtein/LevenshteinTest.java
+++ src/test/java/levenshtein/LevenshteinTest.java
@@ -11,14 +11,53 @@ class LevenshteinTest {
         assertLevenshtein("a", "a", 0);
     }
 
+    /**
+     *  distance = 1
+     * delete
+     * 一个字符的删除
+     * 多个字符删除
+     *
+     * insert
+     * 一个
+     * 多个
+     *
+     * substitute
+     * 一个
+     * 多个
+     *
+     * 综合
+     *
+     *
+     * "kitten" and "sitting"  3
+     *
+     *  kitten
+     *  sitting
+     *
+     *
+     *
+     */
     @Test
     void test_one_character_deletion() {
         assertLevenshtein("ab", "a", 1);
         assertLevenshtein("ab", "b", 1);
         assertLevenshtein("abc", "ac", 1);
         assertLevenshtein("12345678", "1235678", 1);
-//        assertLevenshtein("a", "b", 1);
-//        assertLevenshtein("a", "ab", 1);
+    }
+
+    @Test
+    void test_one_character_insertion() {
+        assertLevenshtein("a", "ab", 1);
+        assertLevenshtein("a", "ac", 1);
+        assertLevenshtein("abc", "abcd", 1);
+        assertLevenshtein("abd", "abcd", 1);
+    }
+
+    @Test
+    void test_one_character_substitution() {
+        assertLevenshtein("a", "b", 1);
+        assertLevenshtein("ab", "ac", 1);
+        assertLevenshtein("abc", "adc", 1);
+        assertLevenshtein("ebcddd", "abcddd", 1);
     }
 
     @Test
